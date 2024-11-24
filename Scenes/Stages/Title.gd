360cabc1ad65f14fce8b3b3c3d437793e0f5be13
PPKRex <josecarlosppk7@gmail.com>
2024-11-24 16:52:47 +0100

    Button Sprite Actualiced

--- Scenes/Stages/Title.gd
+++ Scenes/Stages/Title.gd
@@ -3,7 +3,6 @@ extends Node2D
 @export var animation_node : AnimationPlayer
 
 func _physics_process(delta):
-	print("hola")
 	if (Input.is_action_pressed("button")):
 		animation_node.play("Play")
 	else:
