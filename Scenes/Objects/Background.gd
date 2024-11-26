fe45699bf5d0c0909c50c86e5fe3b5744c1a9935
PPKRex <josecarlosppk7@gmail.com>
2024-11-26 23:51:05 +0100

    Parallax of the background created
    I really don't like the parallax, i think that it will be better with a static background

---
+++ Scenes/Objects/Background.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var sprite:Sprite2D
+@export var timer:Timer
+var backgroundGoingUp = true;
+
+func _physics_process(delta):
+	if backgroundGoingUp:
+		sprite.position = sprite.position - Vector2(0.5,0.1)
+	else:
+		sprite.position = sprite.position - Vector2(0.5,-0.1)
+
+func _on_timer_timeout():
+	backgroundGoingUp = !backgroundGoingUp
