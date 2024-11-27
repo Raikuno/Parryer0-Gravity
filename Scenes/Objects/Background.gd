f1ba9c9b3d65ef65a97d5e16769afc96fd5330e6
PPKRex <josecarlosppk7@gmail.com>
2024-11-27 19:30:58 +0100

    Sprite Changes
    - Added The V4 of the fireball
    - Cleaning of the proyect (I delete de V1, V2 and V3 of the fireball)
    - Fixed a bug in the play button sprite

--- Scenes/Objects/Background.gd
+++ Scenes/Objects/Background.gd
@@ -4,11 +4,11 @@ extends Node2D
 @export var timer:Timer
 var backgroundGoingUp = true;
 
-#func _physics_process(delta):
-#	if backgroundGoingUp:
-#		sprite.position = sprite.position - Vector2(0.5,0.1)
-#	else:
-#		sprite.position = sprite.position - Vector2(0.5,-0.1)
+func _physics_process(delta):
+	if backgroundGoingUp:
+		sprite.position = sprite.position - Vector2(0.5,0.1)
+	else:
+		sprite.position = sprite.position - Vector2(0.5,-0.1)
 
 func _on_timer_timeout():
 	backgroundGoingUp = !backgroundGoingUp
