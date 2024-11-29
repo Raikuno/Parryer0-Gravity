12727c2fcbd6bc1f9855d68370b81c6e918b22bf
Jorge Rojas <rdj160104@gmail.com>
2024-11-29 19:50:59 +0100

    Made Title Screen and improvement in bullets

--- Scripts/Obstacles/Fireball/Fireball.gd
+++ Scripts/Obstacles/Fireball/Fireball.gd
@@ -2,26 +2,33 @@ extends EnemyType
 
 const MIN_SCALE = 1.5
 const MAX_SCALE = 3
-@export var base_velocity_min = 300
-@export var base_velocity_max = 500
+const SLOW = 300
+const MEDIUM = 400
+const FAST = 500
+const SPEEDS = {"SLOW":SLOW, 
+				"MEDIUM":MEDIUM, 
+				"FAST":FAST}
 @export var animation_node : AnimationPlayer
 @export var sprite : Sprite2D
 @export var hitbox_node : CollisionShape2D
-#La velocidad esta completa y absolutamente poseida. Esta esta bautizada y bendecida, no deberia dar problemas
 var object_speed:float
 var y_speed:float
 func _ready():
 	var randomScale = randf_range(MIN_SCALE, MAX_SCALE)
 	scale = Vector2(randomScale, randomScale)
-	
 	#velocity = Vector2(randf_range(base_velocity_min, base_velocity_max), 0.0);
-	object_speed = randf_range(base_velocity_min, base_velocity_max)
+	#object_speed = randf_range(base_velocity_min, base_velocity_max)
 	
 func _physics_process(delta):
 	position.x -= object_speed * delta * Jose_Miguel.get_speed()
 	position.y -= y_speed * delta * Jose_Miguel.get_speed()
 
-
+#Esta funcion se debe llamar desde el objeto que cree a este
+func setSpeed(speed:String):
+	if(speed in SPEEDS.keys()):
+		print("Velocidad no aceptada")
+		return
+	object_speed = SPEEDS[speed]
 
 func _on_hit_player(body:Node2D):
 	object_speed = 0 
@@ -34,8 +41,8 @@ func _on_animation_player_animation_finished(anim_name : String):
 	if anim_name == "explotion" or anim_name == "explotion_parry":
 		destroy()
 
-# Esto esta hecho un poco para testear, son las 4:30 de la mañana, me duele la cabeza y tengo 0 motivo para no dejar esto para mañana
-# Veo bien conservar la mecánica que metí que detecte donde está el jugador y la bala explote en esa dirección, no veo motivo para no dejarla, luego lo hablamos.
+
+#Se necesita implementar la direccion del golpe del jugador
 func _on_enemy_parry(player:My_Player):
 	#var opposite_position = global_position - (body.global_position - global_position)
 	var random = randf_range(-1.0,1.0)
--- Scripts/Obstacles/Fireball/Generator.gd
+++ Scripts/Obstacles/Fireball/Generator.gd
@@ -1,15 +1,57 @@
 extends Path2D
 
+const MAX_SPAWNS = 2
+const NUMBER_OF_POSITIONS = 20.0
+@onready var spawn_chance = randi_range(0, MAX_SPAWNS)
 @export var fireball_scene: PackedScene
 @export var fireballLocation: PathFollow2D
 @export var timer:Timer
 func _on_fireball_timer_timeout():
+	match(spawn_chance):
+		0:
+			pass
+		1:
+			randomMode()
+		2:
+			pattern1()
+			print("pattern 1")
+	spawn_chance = randi_range(0, MAX_SPAWNS)
+
+func generateSingleFireBall(progress:int, speed:String):
 	var fireball = fireball_scene.instantiate()
-	fireballLocation.progress_ratio = randf()
+	fireballLocation.progress_ratio = float(progress / NUMBER_OF_POSITIONS)
+	fireball.position = fireballLocation.position
+	fireball.setSpeed(speed)
+	add_sibling(fireball)
+
+func generateHorde(quantity:int, location:Array, time:Array, speed:Array):
+
+	if !(location.size() == quantity) || !(time.size() == quantity):
+		print("formato incorrecto de posicion/tiempo")
+		return
+	for i in range(0, quantity):
+		await get_tree().create_timer(time[i]).timeout
+		generateSingleFireBall(location[i], speed[i])
+
+#func _on_player_hit():
+#	print("GOLPIADO PUTO")
+
+func randomMode():
+	timer.wait_time = 1.25 - (Jose_Miguel.get_speed()/4)
+	var fireball = fireball_scene.instantiate()
+	fireballLocation.progress_ratio = float(randi_range(0, NUMBER_OF_POSITIONS)/NUMBER_OF_POSITIONS)
 	fireball.position = fireballLocation.position
 	add_sibling(fireball)
 	timer.wait_time = 0.75 - (Jose_Miguel.get_speed()/4)
 
-
-#func _on_player_hit():
-#	print("GOLPIADO PUTO")
+func pattern1():
+	var time:Array
+	var location:Array
+	var quantity:int
+	var speed:Array
+	
+	quantity = 5
+	location = [1, 2, 3, 4, 5]
+	time = [0, 0.1, 0.1, 0.1, 0.1]
+	speed = ["SLOW", "FAST", "SLOW", "MEDIUM", "SLOW"]
+	generateHorde(quantity, location, time, speed)
